86d212963db825266b2c40d12c3d365f43f3ca3e
Anthony Diep <anthonydiep@u.boisestate.edu>
2024-04-15 23:26:52 -0600

    updated

---
+++ profiles/profiles.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    name: "Mr. Froggy", 
+    uri:"https://i.pinimg.com/originals/0f/19/a4/0f19a41875f8162fad5f658dc427a47d.png",
+    bio:"frogging about",
+    nLikes: 0, 
+  },
+  {
+    name: "Hooded Sword Girl", 
+    uri:"https://i.pinimg.com/originals/72/72/11/727211b148f69c5723190ded58755ce0.png",
+    bio:"sword",
+    nLikes: 0, 
+  },
+  {
+    name: "Mrs. Puff", 
+    uri:"https://i.pinimg.com/originals/72/8a/c4/728ac41f011836125b37157d3e582616.png",
+    bio:"spongebob reference",
+    nLikes: 0, 
+  },
+  {
+    name: "Ruma-dum-dum", 
+    uri:"https://i.pinimg.com/originals/24/e6/8c/24e68cefced1031c34df80f232a10601.png", 
+    bio:"big drum",
+    nLikes: 0, 
+  },
+  {
+    name: "Morning Vibes", 
+    uri:"https://i.pinimg.com/originals/00/28/5d/00285dd455af6037dfd4aa9006bba242.png", 
+    bio:"vibing",
+    nLikes: 0, 
+  },
+  {
+    name: "Elf Boy", 
+    uri:"https://i.pinimg.com/originals/88/3e/92/883e923bffdc6cc9b4f94a1e7c2a1fd6.png", 
+    bio:"elf man",
+    nLikes: 1, 
+  }
+]
